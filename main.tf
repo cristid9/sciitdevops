c430343e231966529ea365f98204004fb1c052df
Cristi Dnla <cristi.dnla@gmail.com>
2025-03-24 21:31:48 +0200

    Fix

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_s3_bucket" "bucket" {
-  bucket = var.bucket_name
-  acl    = var.acl
-}
+# resource "aws_s3_bucket" "bucket" {
+#   bucket = var.bucket_name
+#   acl    = var.acl
+# }
