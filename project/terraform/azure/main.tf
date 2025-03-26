a20e1a3cf07dc7ea59964ea16536f512c4135734
Cristi Dnla <cristi.dnla@gmail.com>
2025-03-26 13:00:16 +0200

    Add providers block to azure terraform file

--- project/terraform/azure/main.tf
+++ project/terraform/azure/main.tf
@@ -1,3 +1,15 @@
+provider "azurerm" {
+  features {}
+  subscription_id = var.azure_subscription_id
+  client_id       = var.azure_client_id
+  client_secret   = var.azure_client_secret
+  tenant_id       = var.azure_tenant_id
+}
+
+variable "azure_subscription_id" {}
+variable "azure_client_id" {}
+variable "azure_client_secret" {}
+variable "azure_tenant_id" {}
 
 
 # Resource Group
